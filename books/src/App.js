7dfa1746ce49d5313f3ecb3d772bce0e198606bf
ImranNawar <imran1nawar@gmail.com>
2023-09-10 14:41:45 +0500

    Final Commit

--- books/src/App.js
+++ books/src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  // This function for deletion
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if(book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  // This function for deletion
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  }
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999),
+        title,    //title:title
+      }
+    ];
+    setBooks(updatedBooks);
+  };
+
   return (
-    <div className="App">
-      App
+    <div className='app'>
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}  />
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
