a96f0cc7b2905e92bedaa02dfe7fbe88a66b6817
ImranNawar <imran1nawar@gmail.com>
2023-09-20 22:07:48 +0500

    tailwind css

--- components/src/App.js
+++ components/src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import { BsFillBellFill, BsFillCursorFill, BsFillDatabaseFill } from "react-icons/bs";
 import Button from './components/Button';
+
 const App = () => {
+  const handleClick = () => {
+    console.log("clicked")
+  }
   return (
     <div>
       <div>
-        <Button success rounded outline text="CLick Here"/>
+        <Button secondary outline rounded className="mb-5" onClick={handleClick}>
+          <BsFillBellFill />
+          Click me!!
+        </Button>
       </div>
       <div>
-        <Button danger outline text="Buy Now"/>
+        <Button danger outline>
+        <BsFillCursorFill />
+          Buy Now
+        </Button>
       </div>
       <div>
-        <Button warning text="See Deal"/>
+        <Button warning rounded>
+         <BsFillDatabaseFill />
+          See Deal
+        </Button>
       </div>
       <div>
-        <Button secondary outline text="Hide Adds"/>
+        <Button secondary outline>
+          
+          Hide ads
+        </Button>
       </div>
       <div>
-        <Button secondary rounded text="Something!"/>
+        <Button primary rounded>Something</Button>
       </div>
     </div>
   );
